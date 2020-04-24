3934c4c1ba441f5aade08d5dd627a8c1222d9f55
heerou <jalvarezpimiento@gmail.com>
2020-04-24 17:30:44 -0500

    First Commit

---
+++ Nettoyage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nettoyage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
